3eb7a8687bf87e7c04aba8b718730cd8a2eb3075
Matheus Almeida <48776678+matheusalmsou@users.noreply.github.com>
2023-04-10 22:57:44 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,36 @@
+public class Usuario {
+    
+    private String usuario;
+    private int senha;
+    private String  tipo;
+
+    public Usuario(String usuario, int senha, String tipo) {
+        this.usuario = usuario;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}

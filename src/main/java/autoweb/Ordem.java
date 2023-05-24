be8e67ad8e5c9f3c1b276126204404cefc0e2bfc
Matheus Almeida <matheusalmsou@gmail.com>
2023-05-24 20:46:52 -0300

    Diagrama de classes e classe Ordem atualizados

--- src/main/java/autoweb/Ordem.java
+++ src/main/java/autoweb/Ordem.java
@@ -1,10 +1,61 @@
 package autoweb;
 
+import com.google.protobuf.CodedOutputStream;
+
 public class Ordem {
     
     private int codProd;
     private int codUsuario;
     private int qtd;
-    private int valor;
-    private int total;
+    private double valor;
+    private double total;
+
+    public Ordem(int codProd, int codUsuario, int qtd, double valor, double total) {
+        this.codProd = codProd;
+        this.codUsuario = codUsuario;
+        this.qtd = qtd;
+        this.valor = valor;
+        this.total = total;
+    }
+
+    public void setCodProd(int codProd) {
+        this.codProd = codProd;
+    }
+
+    public int getCodProd() {
+        return codProd;
+    }
+
+    public void setCodUsuario(int codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public int getCodUsuario() {
+        return codUsuario;
+    }
+
+    public void qtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getTotal() {
+        return total;
+    }
 }
+
